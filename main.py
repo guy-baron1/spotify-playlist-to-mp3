f5463bd58f5218d692bb3b0f2db3f3651c19e532
Guy Baron <guyb2000@gmail.com>
2021-11-25 19:11:57 +0200

    small error handling improvements

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from __future__ import unicode_literals
 import json
 import argparse
+from os import error
 import youtube_dl
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+
+failed_downloads = []
 
 def init_args():
     parser = argparse.ArgumentParser(description='Download exported spotify playlist to mp3s from youtube, see github readme for instructions')
@@ -18,23 +21,22 @@ def run(args):
         print(f'Loaded {song_count} songs from input json')
         for index, song in enumerate(parsed_songs['songs']):
             print(f'Downloading song {index}/{song_count}: {song}')
-
             link = find_yt_link(song)
             download_yt_mp3(link, args.output_dir)
+        
+        print(f'Finished, failed to download: {failed_downloads}')
 
 def find_yt_link(query):
-
     words = query.split()
     search_link = 'http://www.youtube.com/results?search_query=' + '+'.join(words)
 
     session = HTMLSession()
     search_result = session.get(search_link)
     search_result.html.render(timeout=20)
-
     soup = BeautifulSoup(search_result.html.html, 'lxml')
     videos = soup.find_all('a', class_= 'yt-simple-endpoint style-scope ytd-video-renderer')
     if not videos:
-        print(f'No video found for {query}')
+        print(f'No video found for query {query}')
 
     return 'https://www.youtube.com' + videos[0]['href']
 
@@ -49,7 +51,12 @@ def download_yt_mp3(link, output_dir):
     }],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
+        try:
+            ydl.cache.remove()
+            ydl.download([link])
+        except youtube_dl.DownloadError:
+            failed_downloads.append(link)
+            pass
 
 if __name__ == '__main__':
     args = init_args()
